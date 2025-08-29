7209f4dc52994f3b1b51261bda7ece13320281ce
nicholiae <ndshaffer92@gmail.com>
2025-08-29 11:41:22 -0400

    Fixing Vehicle deployment
    DeploySCVehicle.c is unnecessary as DeploySCObject works so long as another mod is not modifying the inherited class DeployObject.

--- Scripts/4_World/Entities/ItemBase/VehicleKits/SausageCo_Vehicle_Kit_Base.c
+++ Scripts/4_World/Entities/ItemBase/VehicleKits/SausageCo_Vehicle_Kit_Base.c
@@ -182,7 +182,7 @@ class SausageCo_Vehicle_Kit_Base extends ItemBase
 	{
 		super.SetActions();
 		AddAction(ActionTogglePlaceObject);
-		AddAction(ActionDeploySCVehicle);
+		AddAction(ActionDeploySCObject);
 	}
 };
 class SausageCo_Vehicle_SarkaBase_Kit extends SausageCo_Vehicle_Kit_Base
